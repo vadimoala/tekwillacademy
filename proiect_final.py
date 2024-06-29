727717b875a99999f24e1a23e588e0d586487426
vadimoala <vadimoala@gmail.com>
2024-06-29 16:13:51 +0300

    add icons

--- proiect_final.py
+++ proiect_final.py
@@ -4,6 +4,23 @@ import folium
 from streamlit_folium import folium_static
 
 
+weather_icons = {
+    "cer senin": "https://img.icons8.com/color/96/000000/sun--v1.png",
+    "cer acoperit de nori": "https://img.icons8.com/color/96/000000/clouds.png",
+    "ploaie ușoară": "https://img.icons8.com/color/96/000000/rain--v1.png",
+    "ploaie": "https://img.icons8.com/color/96/000000/heavy-rain.png",
+    "furtună": "https://img.icons8.com/color/96/000000/storm.png",
+    "ninsoare": "https://img.icons8.com/color/96/000000/snow.png",
+    "cețos": "https://img.icons8.com/color/96/000000/fog-day.png",
+    "nori împrăștiați": "https://img.icons8.com/color/96/000000/cloud-computing.png",
+    "dezgheț": "https://img.icons8.com/color/96/000000/temperature.png",
+    "ceață": "https://img.icons8.com/color/96/000000/fog-night.png",
+    "vânt": "https://img.icons8.com/color/96/000000/wind.png",
+    "gheață": "https://img.icons8.com/color/96/000000/iceberg.png",
+    "nori grei": "https://img.icons8.com/color/96/000000/thick-clouds.png",
+    "ploaie deasă": "https://img.icons8.com/color/96/000000/rainfall.png"
+}
+
 def get_weather(city):
     api_key = "d374b0a46ff3fd614ed6c7e315287160"
     base_url = "https://api.openweathermap.org/data/2.5/weather"
@@ -31,6 +48,11 @@ def main():
             st.write(f"În {city_name.capitalize()} este {weather_description} "
                      f"și temperatura este de {temperature:.1f} °C\n"
                      f"\nIar umiditatea este de {umiditatea} %")
+
+            if weather_description in weather_icons:
+                st.image(weather_icons[weather_description], width=50)
+            else:
+                st.write("Nu avem o imagine pentru această stare a vremii.")
 
 
             map = folium.Map(location=[coordinates["lat"], coordinates["lon"]], zoom_start=15)
