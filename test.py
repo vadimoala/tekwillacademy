2d175915bab84315000cfc02de48ae165d3ad81a
vadimoala <vadimoala@gmail.com>
2024-07-01 17:21:59 +0300

    test

--- test.py
+++ test.py
@@ -2,11 +2,27 @@ import streamlit as st
 import requests
 
 
+weather_icons = {
+    "cer senin": "https://img.icons8.com/color/96/000000/sun--v1.png",
+    "cer acoperit de nori": "https://img.icons8.com/color/96/000000/clouds.png",
+    "ploaie ușoară": "https://img.icons8.com/color/96/000000/rain--v1.png",
+    "ploaie": "https://img.icons8.com/color/96/000000/heavy-rain.png",
+    "furtună": "https://img.icons8.com/color/96/000000/storm.png",
+    "ninsoare": "https://img.icons8.com/color/96/000000/snow.png",
+    "cețos": "https://img.icons8.com/color/96/000000/fog-day.png",
+    "nori împrăștiați": "https://img.icons8.com/color/96/000000/cloud-computing.png",
+    "dezgheț": "https://img.icons8.com/color/96/000000/temperature.png",
+    "ceață": "https://img.icons8.com/color/96/000000/fog-night.png",
+    "vânt": "https://img.icons8.com/color/96/000000/wind.png",
+    "gheață": "https://img.icons8.com/color/96/000000/iceberg.png",
+    "nori grei": "https://img.icons8.com/color/96/000000/thick-clouds.png",
+    "ploaie deasă": "https://img.icons8.com/color/96/000000/rainfall.png"
+}
 
 def get_weather(city):
     api_key = "d374b0a46ff3fd614ed6c7e315287160"
     base_url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {"q": city, "appid": api_key, "units": "metric"}
+    params = {"q": city, "appid": api_key, "units": "metric", "lang": "ro"}
 
     response = requests.get(base_url, params=params)
     data = response.json()
@@ -14,20 +30,27 @@ def get_weather(city):
     if response.status_code == 200:
         weather_description = data["weather"][0]["description"]
         temperature = data["main"]["temp"]
+        umiditatea = data["main"]["humidity"]
         coordinates = data["coord"]
-        return weather_description, temperature, coordinates
+        return weather_description, temperature, umiditatea, coordinates
     else:
         return None, None, None
-
 
 def main():
     st.title("Vremea")
     city_name = st.text_input("Introdu orasul dorit:")
 
-    if st.button("Verifică vremea"):
-        weather_description, temperature, coordinates = get_weather(city_name)
-        if weather_description and temperature and coordinates:
-            st.write(f"În {city_name} este {weather_description} și temperatura este de {temperature:.1f} °C.")
+    if st.button("Enter"):
+        weather_description, temperature, umiditatea, coordinates = get_weather(city_name)
+        if weather_description and temperature and umiditatea and coordinates:
+            st.write(f"În {city_name.capitalize()} este {weather_description} "
+                     f"și temperatura este de {temperature:.1f} °C\n"
+                     f"\nIar umiditatea este de {umiditatea} %")
+
+            if weather_description in weather_icons:
+                st.image(weather_icons[weather_description], width=50)
+            else:
+                None
 
 
 
