eb36072d0271b9b5bf25b338ef0b52450fb791d4
vadimoala <vadimoala@gmail.com>
2024-07-01 19:57:48 +0300

    folium

--- streamlit.py
+++ streamlit.py
@@ -1,46 +1,22 @@
 import streamlit as st
-import requests
-import folium
 from streamlit_folium import folium_static
+import folium
 
+"# streamlit-folium"
 
+with st.echo():
+    import streamlit as st
+    from streamlit_folium import folium_static
+    import folium
 
-def get_weather(city):
-    api_key = "d374b0a46ff3fd614ed6c7e315287160"
-    base_url = "https://api.openweathermap.org/data/2.5/weather"
-    params = {"q": city, "appid": api_key, "units": "metric"}
+    # center on Liberty Bell
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
 
-    response = requests.get(base_url, params=params)
-    data = response.json()
+    # add marker for Liberty Bell
+    tooltip = "Liberty Bell"
+    folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+    ).add_to(m)
 
-    if response.status_code == 200:
-        weather_description = data["weather"][0]["description"]
-        temperature = data["main"]["temp"]
-        coordinates = data["coord"]
-        return weather_description, temperature, coordinates
-    else:
-        return None, None, None
-
-
-def main():
-    st.title("Vremea")
-    city_name = st.text_input("Introdu orasul dorit:")
-
-    if st.button("Verifică vremea"):
-        weather_description, temperature, coordinates = get_weather(city_name)
-        if weather_description and temperature and coordinates:
-            st.write(f"În {city_name} este {weather_description} și temperatura este de {temperature:.1f} °C.")
-
-
-            map = folium.Map(location=[coordinates["lat"], coordinates["lon"]], zoom_start=10)
-            folium.Marker([coordinates["lat"], coordinates["lon"]], popup=f"{city_name} - {temperature:.1f} °C").add_to(
-                map)
-
-
-            folium_static(map)
-        else:
-            st.write("Nu am putut obține datele meteo pentru acest oraș.")
-
-
-if __name__ == "__main__":
-    main()
+    # call to render Folium map in Streamlit
+    folium_static(m)
